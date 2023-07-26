234366313f6971f8d61ee21460550e19a1b1c74a
Abdullah Imran <125476881+Craftzmen@users.noreply.github.com>
2023-07-26 19:52:25 +0500

    Taskly Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js, jsx, ts, tsx, svelte}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'sm-md': '13px'
+      },
+      width: {
+        '4.5' : '1.15rem'
+      },
+      height: {
+        '4.5' : '1.15rem'
+      }
+    },
   },
   plugins: [],
 }
